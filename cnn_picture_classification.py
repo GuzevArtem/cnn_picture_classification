b8e65aa91e9723df37935ada08a67647d2dab9a0
GuzevArtem\Beshinei <guzev.artem@gmail.com>
2020-12-21 17:10:58 +0200

    Add project files.

---
+++ cnn_picture_classification.py
@@ -0,0 +1,50 @@
+
+import tensorflow as tf
+
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
+# Load from https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz ETA 5-7 min, cached automatically
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+
+# Model structure
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64 * 9, activation='relu'))
+model.add(layers.Dense(64 * 3, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(10)) #according to cifar number of outputs
+
+# Model compilation
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# Model training
+history = model.fit(train_images, train_labels, epochs=15, 
+                    validation_data=(test_images, test_labels))
+
+# Accuracy history graph
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
+plt.show()
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print(test_acc)
